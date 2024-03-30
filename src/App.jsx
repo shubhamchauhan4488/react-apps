5213e5472d4df997113f73052adb6262619f0cf1
SHUBHAM CHAUHAN <shubhamchauhan4488@gmail.com>
2024-03-30 22:57:20 +0000

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
-import { useState } from 'react'
+import { Outlet } from "react-router-dom";
 import reactLogo from './assets/react.svg'
+import reactRouterLogo from './assets/react-router-logo.webp'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Counter } from './modules/counter/counter'
 
-function App() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Dashboard = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="layout">
+      <div className="sidebar">
+        <h2>React Apps</h2>
+        <ul>
+          <li><Link to="/counter">Counter</Link></li>
+          <li><Link to="/imdb">IMDB</Link></li>
+          <li><Link to="/rating">Rating</Link></li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <Counter />
-    </>
-  )
-}
+      <div className='dashboard'>
+        <div className='banner'>
+          <div>
+            <a href="https:vitejs.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https:react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+            <a href="https:reactrouter.com/en/main" target="_blank">
+              <img src={reactRouterLogo} className="logo react" alt="React Router logo" />
+            </a>
+          </div>
+        </div>
+        <div id="detail">
+          <Outlet />
+        </div>
+      </div>
+    </div>
 
-export default App
+  );
+};
+
+export default Dashboard
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './ErrorPage.jsx';
+import { Counter } from './modules/counter/counter.jsx';
+import { Movies } from './modules/imdb/movies.jsx';
+import { Rating } from './modules/rating/rating.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "counter",
+        element: <Counter />,
+      },
+      {
+        path: "imdb",
+        element: <Movies />,
+      },
+      {
+        path: "rating",
+        element: <Rating ratingLength={4}/>,
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
